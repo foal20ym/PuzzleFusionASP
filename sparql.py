5756831e8a85d9166b37662765a7e824deac4ca1
Alexander Forsanker <alexander-forsanker2011@hotmail.com>
2025-01-09 11:54:11 +0100

    Removed commented code and print statements

--- sparql.py
+++ sparql.py
@@ -8,36 +8,9 @@ def load_questions():
         config = yaml.safe_load(file)
     return config["questions"]
 
-# Fetch all bands from YAGO
-#def get_all(type):
-#    sparql = SPARQLWrapper("https://yago-knowledge.org/sparql/query")
-#    sparql.setReturnFormat(JSON)
-#    sparql.setQuery("""
-#        PREFIX schema: <http://schema.org/>
-#        PREFIX yago: <http://yago-knowledge.org/resource/>
-#        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#        SELECT DISTINCT ?band 
-#        WHERE {
-#            ?band a schema:MusicGroup .
-#            ?person schema:memberOf ?band .
-#        }
-#    """
-#    )
-#
-#    response = sparql.queryAndConvert()
-#    bands = [result["band"]["value"] for result in response["results"]["bindings"]]
-#    print(bands)
-#    return bands
-
 def get_all(tpe, prop):
     sparql = SPARQLWrapper("https://yago-knowledge.org/sparql/query")
     sparql.setReturnFormat(JSON)
-    print(tpe, prop)
-    #extra = ""
-    #if len(tpe > 1):
-    #    for i in range(1, len(tpe)):
-    #        extra += f"UNION{{?thing a yago:{tpe[i]} . }}"
     sparql.setQuery(f"""
         PREFIX schema: <http://schema.org/>
         PREFIX yago: <http://yago-knowledge.org/resource/>
@@ -53,7 +26,6 @@ def get_all(tpe, prop):
     response = sparql.queryAndConvert()
     things = [result["thing"]["value"] for result in response["results"]["bindings"]]
     filtered = [s for s in things if ("u0028" or "u0029") not in s]
-    print(filtered)
     return filtered
 
 def get_query(question, entity):
@@ -106,7 +78,6 @@ def get_query(question, entity):
         """
     
 
-# Formulate a question about the selected band and fetch the answer
 def get_answer():
 
     questions = load_questions()
@@ -116,30 +87,22 @@ def get_answer():
     q_type = question["type"]
     q_property = question["property"]
 
-    print(q_text, q_type, q_property)
-
     entities = get_all(q_type, q_property)
 
     results = []
     while not results:
         entity = random.choice(entities)
         entity = entity.split('/')[-1]
-        print(entity)
 
         sparql = SPARQLWrapper("https://yago-knowledge.org/sparql/query")
         sparql.setReturnFormat(JSON)
 
         formulated_question = q_text.replace("?", entity.replace("_", " ")) + "?"
-        print(formulated_question)
 
         q = get_query(q_text, entity)
-        print(q)
         sparql.setQuery(q)
         response = sparql.queryAndConvert()
         results = response["results"]["bindings"]
-
-    print(response)
-    print(results)
 
     if "count" in results[0]:
         return f"{results[0]['count']['value']} members", formulated_question
